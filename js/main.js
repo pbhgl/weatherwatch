6b22acfbfd83b77b6dd6a062ce20fe18e28c54a5
pbhgl <pbhgl@users.noreply.github.com>
2015-12-29 20:45:33 +0100

    more settings added

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
+var versionLabel = 100;
+
 (function() {
   loadOptions();
   setHandlers();
@@ -7,6 +9,22 @@ function setHandlers() {
   var $apiKey = $('#apiKey');
   $apiKey.on('change', function() {
     $apiKey[0].value = $apiKey[0].value.replace(/\W/g, "");
+  });
+
+  var $gps = $('#gps');
+  $gps.on('change', function() {
+    var $latlonSettings = $('#latlon-settings');
+    if ($gps[0].checked) {
+      $latlonSettings.css('display', 'none');
+    }
+  });
+
+  var $latlon = $('#latlon');
+  $latlon.on('change', function() {
+    var $latlonSettings = $('#latlon-settings');
+    if ($latlon[0].checked) {
+      $latlonSettings.css('display', '');
+    }
   });
 
   var $latitude = $('#latitude');
@@ -19,29 +37,65 @@ function setHandlers() {
     $longitude[0].value = $longitude[0].value.replace(/[^0-9.+-]/g, "");
   });
 
-  var $submitButton = $('#submitButton');
-  $submitButton.on('click', function() {
-    console.log('Submit');
+  var $reloadDefaultColors = $('#reloadDefaultColors');
+  $reloadDefaultColors.on('click', function() {
+    console.log('Reload Default Colors');
 
-    var return_to = getQueryParam('return_to', 'pebblejs://close#');
-    document.location = return_to + encodeURIComponent(JSON.stringify(getAndStoreConfigData()));
+    localStorage.colorBackground = '0x000000';
+    localStorage.colorText = '0xFFFFFF';
+    localStorage.colorHourHand = '0xFFFFFF';
+    localStorage.colorMinuteHand = '0x55AAFF';
+    localStorage.colorMinuteHandNoBT = '0xFF5500';
+    localStorage.colorHourMarkers = '0xFFFFFF';
+    localStorage.colorMinorMarkers = '0xFFFFFF';
+    location.reload();
   });
+
+  var $submitButton = $('#submitButton');
+  $submitButton.on('click', submit);
+
+  var $submitButton2 = $('#submitButton2');
+  $submitButton2.on('click', submit);
+}
+
+function submit() {
+  console.log('Submit');
+
+  var return_to = getQueryParam('return_to', 'pebblejs://close#');
+  document.location = return_to + encodeURIComponent(JSON.stringify(getAndStoreConfigData()));
 }
 
 function loadOptions() {
+  var callerVersion = parseInt(getQueryParam('version', 0));
+  var storageVersion = localStorage.versionLabel;
+  
+  var platform = getQueryParam('platform', 'chalk');
+  if (platform == 'aplite') {
+    $colorSettings = $('#colorSettings');
+    $colorSettings.css('display', 'none');
+  }
+
   var $apiKey = $('#apiKey');
   if (localStorage.apiKey) {
     $apiKey[0].value = localStorage.apiKey;
   }
 
   var $gps = $('#gps');
-  if (localStorage.gps) {
-    $gps[0].value = localStorage.gps;
-  }
-
   var $latlon = $('#latlon');
-  if (localStorage.latlon) {
-    $latlon[0].value = localStorage.latlon;
+  var $latitude = $('#latitude');
+  var $longitude = $('#longitude');
+  var $latlonSettings = $('#latlon-settings');
+  if (localStorage.useLatLon) {
+    if (localStorage.useLatLon === 'true') {
+      $gps[0].checked = false;
+      $latlon[0].checked = true;
+    } else {
+      $gps[0].checked = true;
+      $latlon[0].checked = false;
+    }
+  }
+  if ($gps[0].checked) {
+    $latlonSettings.css('display', 'none');
   }
 
   var $latitude = $('#latitude');
@@ -59,24 +113,39 @@ function loadOptions() {
     $frequency[0].value = localStorage.frequency;
   }
 
-  var $showMoonPhase = $('#showMoonPhase');
-  if (localStorage.showMoonPhase) {
-    $showMoonPhase[0].checked = localStorage.showMoonPhase === 'true';
+  var $displayLocation = $('#displayLocation');
+  if (localStorage.displayLocation) {
+    $displayLocation[0].checked = localStorage.displayLocation === 'true';
   }
 
-  var $showLocation = $('#showLocation');
-  if (localStorage.showLocation) {
-    $showLocation[0].checked = localStorage.showLocation === 'true';
+  var $displayHourMarkers = $('#displayHourMarkers');
+  if (localStorage.displayHourMarkers) {
+    $displayHourMarkers[0].checked = localStorage.displayHourMarkers === 'true';
   }
 
-  var $showUpdateTime = $('#showUpdateTime');
-  if (localStorage.showUpdateTime) {
-    $showUpdateTime[0].checked = localStorage.showUpdateTime === 'true';
+  var $displayMinorMarkers = $('#displayMinorMarkers');
+  if (localStorage.displayMinorMarkers) {
+    $displayMinorMarkers[0].checked = localStorage.displayMinorMarkers === 'true';
+  }
+
+  var $displayObservationTime = $('#displayObservationTime');
+  if (localStorage.displayObservationTime) {
+    $displayObservationTime[0].checked = localStorage.displayObservationTime === 'true';
+  }
+
+  var $displayUpdateTime = $('#displayUpdateTime');
+  if (localStorage.displayUpdateTime) {
+    $displayUpdateTime[0].checked = localStorage.displayUpdateTime === 'true';
   }
 
   var $colorBackground = $('#colorBackground');
   if (localStorage.colorBackground) {
     $colorBackground[0].value = localStorage.colorBackground;
+  }
+
+  var $colorText = $('#colorText');
+  if (localStorage.colorText) {
+    $colorText[0].value = localStorage.colorText;
   }
 
   var $colorHourHand = $('#colorHourHand');
@@ -88,6 +157,21 @@ function loadOptions() {
   if (localStorage.colorMinuteHand) {
     $colorMinuteHand[0].value = localStorage.colorMinuteHand;
   }
+
+  var $colorMinuteHandNoBT = $('#colorMinuteHandNoBT');
+  if (localStorage.colorMinuteHandNoBT) {
+    $colorMinuteHandNoBT[0].value = localStorage.colorMinuteHandNoBT;
+  }
+
+  var $colorHourMarkers = $('#colorHourMarkers');
+  if (localStorage.colorHourMarkers) {
+    $colorHourMarkers[0].value = localStorage.colorHourMarkers;
+  }
+
+  var $colorMinorMarkers = $('#colorMinorMarkers');
+  if (localStorage.colorMinorMarkers) {
+    $colorMinorMarkers[0].value = localStorage.colorMinorMarkers;
+  }
 }
 
 function getAndStoreConfigData() {
@@ -97,38 +181,58 @@ function getAndStoreConfigData() {
   var $latitude = $('#latitude');
   var $longitude = $('#longitude');
   var $frequency = $('#frequency');
-  var $showMoonPhase = $('#showMoonPhase');
-  var $showLocation = $('#showLocation');
-  var $showUpdateTime = $('#showUpdateTime');
+  var $displayLocation = $('#displayLocation');
+  var $displayHourMarkers = $('#displayHourMarkers');
+  var $displayMinorMarkers = $('#displayMinorMarkers');
+  var $displayObservationTime = $('#displayObservationTime');
+  var $displayUpdateTime = $('#displayUpdateTime');
   var $colorBackground = $('#colorBackground');
+  var $colorText = $('#colorText');
   var $colorHourHand = $('#colorHourHand');
   var $colorMinuteHand = $('#colorMinuteHand');
+  var $colorMinuteHandNoBT = $('#colorMinuteHandNoBT');
+  var $colorHourMarkers = $('#colorHourMarkers');
+  var $colorMinorMarkers = $('#colorMinorMarkers');
 
-  var options = {
+  var options;
+  options = {
     apiKey: $apiKey.val().replace(/\W/g, ""),
     useLatLon: $latlon[0].checked,
     latitude: $latitude.val(),
     longitude: $longitude.val(),
     frequency: $frequency.val(),
-    showMoonPhase: $showMoonPhase[0].checked,
-    showLocation: $showLocation[0].checked,
-    showUpdateTime: $showUpdateTime[0].checked,
+    displayLocation: $displayLocation[0].checked,
+    displayHourMarkers: $displayHourMarkers[0].checked,
+    displayMinorMarkers: $displayMinorMarkers[0].checked,
+    displayObservationTime: $displayObservationTime[0].checked,
+    displayUpdateTime: $displayUpdateTime[0].checked,
     colorBackground: $colorBackground.val(),
+    colorText: $colorText.val(),
     colorHourHand: $colorHourHand.val(),
-    colorMinuteHand: $colorMinuteHand.val()
+    colorMinuteHand: $colorMinuteHand.val(),
+    colorMinuteHandNoBT: $colorMinuteHandNoBT.val(),
+    colorHourMarkers: $colorHourMarkers.val(),
+    colorMinorMarkers: $colorMinorMarkers.val()
   };
 
+  localStorage.versionLabel = versionLabel;
   localStorage.apiKey = options.apiKey;
   localStorage.useLatLon = options.useLatLon;
   localStorage.latitude = options.latitude;
   localStorage.longitude = options.longitude;
   localStorage.frequency = options.frequency;
-  localStorage.showMoonPhase = options.showMoonPhase;
-  localStorage.showLocation = options.showLocation;
-  localStorage.showUpdateTime = options.showUpdateTime;
+  localStorage.displayLocation = options.displayLocation;
+  localStorage.displayHourMarkers = options.displayHourMarkers;
+  localStorage.displayMinorMarkers = options.displayMinorMarkers;
+  localStorage.displayObservationTime = options.displayObservationTime;
+  localStorage.displayUpdateTime = options.displayUpdateTime;
   localStorage.colorBackground = options.colorBackground;
+  localStorage.colorText = options.colorText;
   localStorage.colorHourHand = options.colorHourHand;
   localStorage.colorMinuteHand = options.colorMinuteHand;
+  localStorage.colorMinuteHandNoBT = options.colorMinuteHandNoBT;
+  localStorage.colorHourMarkers = options.colorHourMarkers;
+  localStorage.colorMinorMarkers = options.colorMinorMarkers;
 
   console.log('Got options: ' + JSON.stringify(options));
   return options;
